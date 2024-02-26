3dfae02bb8a951a54d4bacfcf0dfbad061a33c2e
Predator <muradforgame11@gmail.com>
2024-02-26 13:10:10 -0800

    Sidebar media query fixed :building_construction: and Home page added :memo:

--- src/_root/RootLayout.jsx
+++ src/_root/RootLayout.jsx
@@ -18,28 +18,106 @@ function RootLayout() {
   const navigate = useNavigate();
 
   return (
-    <div className="flex">
-      <div className="w-[300px] bg-colSors-color1 flex items-center justify-center bg-colors-color1 h-screen">
-        <div className="h-[95%] flex flex-col justify-between w-fit">
-          <div className="w-fit flex flex-col gap-[40px] mt-[10px]">
-            <div
-              className="flex items-center w-fit gap-2"
+    <div className="flex flex-col max-[800px]:bg-colors-color1">
+      <div className="w-screen fixed hidden max-[1000px]:block">
+        <div className="flex items-center bg-colors-color1 h-[60px] justify-center">
+          <div className="flex w-[92%] justify-between">
+            <div className="flex items-center gap-1">
+              <img src={img1} className="w-[40px]" />
+              <h1 className="text-[22px] text-colors-color3 font-bold">
+                Streaminny
+              </h1>
+            </div>
+            <div className="flex items-center gap-[1.30rem]">
+              <img
+                src={img2}
+                className="w-[45px] rounded-[50%] object-cover max-[550px]:w-[40px]"
+              />
+              <IoMdExit className="text-[32px] text-colors-color3 max-[550px]:text-[28px]" />
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center bg-colors-color1 fixed bottom-0 left-0 h-[60px] w-screen">
+          <ul className="flex h-[100%] items-center justify-between w-[90%]">
+            <li
+              className="p-[9px] rounded-[8px] hover:bg-[#211e21]"
+              style={{
+                backgroundColor: res == "/home" || res == "/" ? "#8685ff" : "",
+              }}
             >
+              <BsFillHouseFill
+                className="text-[32px] text-colors-color3 max-[550px]:text-[20px]"
+                style={{
+                  color: res == "/home" || res == "/" ? "white" : "#8685ff",
+                }}
+                onClick={() => navigate("/home")}
+              />
+            </li>
+            <li
+              className="p-[9px] rounded-[8px] hover:bg-[#211e21]"
+              style={{ backgroundColor: res == "/explore" ? "#8685ff" : "" }}
+            >
+              <MdExplore
+                className="text-[32px] text-colors-color3 max-[550px]:text-[20px]"
+                style={{ color: res == "/explore" ? "white" : "#8685ff" }}
+                onClick={() => navigate("/explore")}
+              />
+            </li>
+            <li
+              className="p-[9px] rounded-[8px] hover:bg-[#211e21]"
+              style={{ backgroundColor: res == "/friends" ? "#8685ff" : "" }}
+            >
+              <FaUserFriends
+                className="text-[32px] text-colors-color3 max-[550px]:text-[20px]"
+                style={{ color: res == "/friends" ? "white" : "#8685ff" }}
+                onClick={() => navigate("/friends")}
+              />
+            </li>
+            <li
+              className="p-[9px] rounded-[8px] hover:bg-[#211e21]"
+              style={{ backgroundColor: res == "/saved" ? "#8685ff" : "" }}
+            >
+              <HiOutlineSave
+                className="text-[32px] text-colors-color3 max-[550px]:text-[20px]"
+                style={{ color: res == "/saved" ? "white" : "#8685ff" }}
+                onClick={() => navigate("/saved")}
+              />
+            </li>
+            <li
+              className="p-[9px] rounded-[8px] hover:bg-[#211e21]"
+              style={{
+                backgroundColor: res == "/create-post" ? "#8685ff" : "",
+              }}
+            >
+              <MdAddPhotoAlternate
+                className="text-[32px] text-colors-color3 max-[550px]:text-[20px]"
+                style={{ color: res == "/create-post" ? "white" : "#8685ff" }}
+                onClick={() => navigate("/create-post")}
+              />
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div
+        className=" fixed w-[300px] bg-colSors-color1 flex items-center justify-center bg-colors-color1 h-screen max-[1590px]:w-[120px]
+      max-[1000px]:hidden"
+      >
+        <div className="h-[95%] flex flex-col justify-between w-fit">
+          <div className="w-fit flex flex-col gap-[40px] mt-[10px] max-[1590px]:items-center">
+            <div className="flex items-center w-fit gap-2">
               <img src={img1} className="w-[52px]" />
-              <h1
-                className="text-[28px] font-bold text-colors-color3"
-              >
+              <h1 className="text-[28px] font-bold text-colors-color3 max-[1590px]:hidden">
                 Streaminny
               </h1>
             </div>
             <div className="flex gap-2 w-fit ">
               <img src={img2} className="w-[54px] rounded-full object-cover " />
-              <div className="flex flex-col justify-center">
+              <div className="flex flex-col justify-center max-[1590px]:hidden">
                 <h1 className="text-xl font-bold ">Murad0012</h1>
                 <h3 className="text-gray-500 ">@murad0012</h3>
               </div>
             </div>
-            <ul className="flex flex-col gap-[20px] w-[260px] ">
+            <ul className="flex flex-col gap-[20px] w-[260px] max-[1590px]:w-[65px] ">
               <li
                 className="flex rounded-[8px] p-4 gap-4 transtion duration-400 hover:bg-[#211e21] "
                 onClick={() => navigate("/home")}
@@ -54,7 +132,7 @@ function RootLayout() {
                     color: res == "/home" || res == "/" ? "" : "#8685ff",
                   }}
                 />
-                <h1 className="text-xl">Home</h1>
+                <h1 className="text-xl max-[1590px]:hidden">Home</h1>
               </li>
               <li
                 className="flex rounded-[8px] p-4 gap-4 hover:bg-[#211e21] "
@@ -65,7 +143,7 @@ function RootLayout() {
                   className="text-[32px]"
                   style={{ color: res == "/explore" ? "" : "#8685ff" }}
                 />
-                <h1 className="text-xl">Explore</h1>
+                <h1 className="text-xl max-[1590px]:hidden">Explore</h1>
               </li>
               <li
                 className="flex rounded-[8px] p-4 gap-4 hover:bg-[#211e21] "
@@ -76,7 +154,7 @@ function RootLayout() {
                   className="text-[28px]"
                   style={{ color: res == "/friends" ? "" : "#8685ff" }}
                 />
-                <h1 className="text-xl">Friends</h1>
+                <h1 className="text-xl max-[1590px]:hidden">Friends</h1>
               </li>
               <li
                 className="flex rounded-[8px] p-4 gap-4 hover:bg-[#211e21] "
@@ -87,7 +165,7 @@ function RootLayout() {
                   className="text-[32px]"
                   style={{ color: res == "/saved" ? "" : "#8685ff" }}
                 />
-                <h1 className="text-xl">Saved</h1>
+                <h1 className="text-xl max-[1590px]:hidden">Saved</h1>
               </li>
               <li
                 className="flex rounded-[8px] p-4 gap-4 hover:bg-[#211e21]"
@@ -100,13 +178,13 @@ function RootLayout() {
                   className="text-[32px]"
                   style={{ color: res == "/create-post" ? "" : "#8685ff" }}
                 />
-                <h1 className="text-xl">Create</h1>
+                <h1 className="text-xl max-[1590px]:hidden">Create</h1>
               </li>
             </ul>
           </div>
           <div className="flex rounded-[8px] gap-4 p-4 hover:bg-[#211e21]">
             <IoMdExit className="text-[32px] text-colors-color3" />
-            <h1 className="text-xl">Logout</h1>
+            <h1 className="text-xl max-[1590px]:hidden">Logout</h1>
           </div>
         </div>
       </div>
--- src/_root/pages/Home.jsx
+++ src/_root/pages/Home.jsx
@@ -1,11 +1,189 @@
-import React from 'react'
+import React from "react";
+
+import img3 from "../imgs/Netflix logo.jpg";
+import img4 from "../imgs/netflixPost.png";
+import img5 from "../imgs/Xbox Logo.jpg";
+import img6 from "../imgs/XboxPost.png";
+import img7 from "../imgs/Jlogo.jpg";
+import img8 from "../imgs/blenderlogo.jpg";
+import img9 from "../imgs/blenderpost.png";
+
+import { FaHeart } from "react-icons/fa";
+import { HiOutlineSave } from "react-icons/hi";
 
 function home() {
   return (
-    <div>
-      <h1 className='text-white'>Home</h1>
+    <div className="w-auto min-h-screen h-fit ml-[300px] flex justify-center max-[1590px]:ml-[120px] max-[1000px]:ml-0 max-[1000px]:mt-[60px]">
+      <div className="flex max-[1000px]:pt-[20px] pb-[100px]">
+        <div className=" w-[850px] flex flex-col items-center max-[1000px]:w-fit">
+          <div className="w-[850px] my-8 pl-[85px] max-[1000px]:hidden">
+            <h1 className="text-[30px] font-bold">Home Feed</h1>
+          </div>
+          <div className="w-[850px] h-max flex flex-col items-center gap-[50px] max-[1000px]:w-fit">
+            <div
+              className="w-[680px]  bg-colors-color1 rounded-[10px] flex flex-col gap-[20px] p-[1rem]
+            max-[800px]:w-[500px] max-[550px]:p-0 max-[550px]:w-screen"
+            >
+              <div className="flex flex-col gap-[15px] w-fit">
+                <div className="flex gap-[8px] w-100% max-[550px]:pl-[10px] items-center">
+                  <img
+                    src={img3}
+                    className="w-[54px] h-[54px] rounded-[50%] object-cover max-[550px]:w-[45px] max-[550px]:h-[45px]"
+                  />
+                  <div className="flex flex-col justify-center">
+                    <h1 className="text-[24px] font-bold max-[550px]:text-[19px]">
+                      Netflix
+                    </h1>
+                    <p className="text-[14px] text-gray-500 max-[550px]:text-[12px]">
+                      3 days ago
+                    </p>
+                  </div>
+                </div>
+                <div className="w-100% max-[550px]:pl-[18px]">
+                  <h1 className="text-[20px] font-bold max-[550px]:text-[18px]">
+                    Thank me later
+                  </h1>
+                  <div className="flex gap-2">
+                    <p className="text-[14px] text-gray-500">#movie</p>
+                    <p className="text-[14px] text-gray-500">#neflix</p>
+                    <p className="text-[14px] text-gray-500">#family</p>
+                  </div>
+                </div>
+                <img
+                  src={img4}
+                  className="w-[650px] object-cover max-[800px]:w-[480px] max-[550px]:w-[100%] rounded-none"
+                />
+                <div className="flex justify-between w-100% max-[550px]:px-[20px]">
+                  <FaHeart className="text-[28px]" />
+                  <HiOutlineSave className="text-[28px]" />
+                </div>
+              </div>
+            </div>
+            <hr className="w-[95%]" />
+            <div
+              className="w-[680px]  bg-colors-color1 rounded-[10px] flex flex-col gap-[20px] p-[1rem]
+            max-[800px]:w-[500px] max-[550px]:p-0 max-[550px]:w-screen"
+            >
+              <div className="flex flex-col gap-[15px] w-fit">
+                <div className="flex gap-[8px] w-100% max-[550px]:pl-[10px] items-center">
+                  <img
+                    src={img5}
+                    className="w-[54px] h-[54px] rounded-[50%] object-cover max-[550px]:w-[45px] max-[550px]:h-[45px]"
+                  />
+                  <div className="flex flex-col justify-center">
+                    <h1 className="text-[24px] font-bold max-[550px]:text-[19px]">
+                      Xbox
+                    </h1>
+                    <p className="text-[14px] text-gray-500 max-[550px]:text-[12px]">
+                      1 days ago
+                    </p>
+                  </div>
+                </div>
+                <div className="w-100% max-[550px]:pl-[18px]">
+                  <h1 className="text-[20px] font-bold max-[550px]:text-[18px]">
+                    ⚔️ Avowed ⚔️ Coming to Xbox Series X|S, PC, and Game Pass
+                    Fall 2024 |
+                  </h1>
+                  <div className="flex gap-2">
+                    <p className="text-[14px] text-gray-500">
+                      #DeveloperDirect
+                    </p>
+                    <p className="text-[14px] text-gray-500">#xbox</p>
+                  </div>
+                </div>
+                <img
+                  src={img6}
+                  className="w-[650px] object-cover max-[800px]:w-[480px] max-[550px]:w-[100%] rounded-none"
+                />
+                <div className="flex justify-between w-100% max-[550px]:px-[20px]">
+                  <FaHeart className="text-[28px]" />
+                  <HiOutlineSave className="text-[28px]" />
+                </div>
+              </div>
+            </div>
+            <hr className="w-[95%]" />
+            <div
+              className="w-[680px]  bg-colors-color1 rounded-[10px] flex flex-col gap-[20px] p-[1rem]
+            max-[800px]:w-[500px] max-[550px]:p-0 max-[550px]:w-screen"
+            >
+              <div className="flex flex-col gap-[15px] w-fit">
+                <div className="flex gap-[8px] w-100% max-[550px]:pl-[10px] items-center">
+                  <img
+                    src={img8}
+                    className="w-[54px] h-[54px] rounded-[50%] object-cover max-[550px]:w-[45px] max-[550px]:h-[45px]"
+                  />
+                  <div className="flex flex-col justify-center">
+                    <h1 className="text-[24px] font-bold max-[550px]:text-[19px]">
+                      Blender
+                    </h1>
+                    <p className="text-[14px] text-gray-500 max-[550px]:text-[12px]">
+                      1 hours ago
+                    </p>
+                  </div>
+                </div>
+                <div className="w-100% max-[550px]:pl-[18px]">
+                  <h1 className="text-[20px] font-bold max-[550px]:text-[18px]">
+                    The CHARGE ⚡️open movie by Blender Studio is live on
+                    YouTube - Check it out!{" "}
+                  </h1>
+                  <div className="flex gap-2">
+                    <p className="text-[14px] text-gray-500">#b3d</p>
+                    <p className="text-[14px] text-gray-500">#blender</p>
+                    <p className="text-[14px] text-gray-500">#animation</p>
+                  </div>
+                </div>
+                <img
+                  src={img9}
+                  className="w-[650px] object-cover max-[800px]:w-[480px] max-[550px]:w-[100%] rounded-none"
+                />
+                <div className="flex justify-between w-100% max-[550px]:px-[20px]">
+                  <FaHeart className="text-[28px]" />
+                  <HiOutlineSave className="text-[28px]" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className=" flex flex-col max-[1410px]:hidden">
+          <div className="my-8">
+            <h1 className="text-[30px] font-bold">Make New Friends</h1>
+          </div>
+          <div className="flex flex-col gap-[25px]">
+            <div
+              className="flex gap-[14px] items-center w-[410px] h-[82px] bg-colors-color1 pl-[10px] rounded-[8px] border border-colors-color1
+            transition-all duration-200 hover:border-colors-color3"
+            >
+              <img src={img3} className="rounded-[50%] w-[50px] object-cover" />
+              <h1 className="text-[18px] font-bold w-[220px]">Netflix</h1>
+              <button className="bg-colors-color3 px-[16px] py-[7px] rounded-[5px] transition-all duration-400 hover:opacity-65">
+                Follow
+              </button>
+            </div>
+            <div
+              className="flex gap-[14px] items-center w-[410px] h-[82px] bg-colors-color1 pl-[10px] rounded-[8px] border border-colors-color1
+            transition-all duration-200 hover:border-colors-color3"
+            >
+              <img src={img5} className="rounded-[50%] w-[50px] object-cover" />
+              <h1 className="text-[18px] font-bold w-[220px]">Xbox</h1>
+              <button className="bg-colors-color3 px-[16px] py-[7px] rounded-[5px] transition-all duration-400 hover:opacity-65">
+                Follow
+              </button>
+            </div>
+            <div
+              className="flex gap-[14px] items-center w-[410px] h-[82px] bg-colors-color1 pl-[10px] rounded-[8px] border border-colors-color1
+            transition-all duration-200 hover:border-colors-color3"
+            >
+              <img src={img7} className="rounded-[50%] w-[50px] object-cover" />
+              <h1 className="text-[18px] font-bold w-[220px]">Junemeniz_23</h1>
+              <button className="bg-colors-color3 px-[16px] py-[7px] rounded-[5px] transition-all duration-400 hover:opacity-65">
+                Follow
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default home
+export default home;
